329b77704e5346295c01883df69a82c24cdba2c3
zeikdev <78682506+zeikdev@users.noreply.github.com>
2023-04-09 22:19:57 -0300

    Add files via upload

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,26 +4,27 @@ import { SiHtml5, SiCss3, SiJavascript, SiBootstrap, SiSass, SiReact, SiNodedotj
 const About = () => {
     return (
 
-        <div className="container flex flex-col gap-12 justify-center md:grid grid-rows-2 grid-cols-2 grid-flow-row md:gap-8 md:items-start">
+        <div className="container flex flex-col md:flex-row md:flex-wrap justify-center md:justify-start gap-y-12">
 
-            <div className="flex flex-col items-start justify-center gap-12 md:gap-6 md:row-span-1">
-                <h2 className='text-white text-3xl font-semibold border-b-4 border-purple-500 pb-2'>Sobre mi.</h2>
+            <div className='flex flex-col items-start justify-center gap-12 md:order-1 md:w-6/12 md:gap-6'>
 
+                <h2 className='text-white text-2xl font-semibold border-b-4 border-purple-500 pb-2 self-start'>Sobre mi.</h2>
                 <p className='text-white text-xl'>Trabajo con tecnologías como JavaScript, React, Next.js, TypeScript, HTML, CSS, Tailwind, Sass, entre otras.</p>
-
                 <p className='text-white text-xl'>Hice cursos en Platzi, Udemy, LeonidasEsteban y Coderhouse.</p>
 
             </div>
 
-            <div className="flex items-center justify-center row-span-2">
-                <img src="./diploma.png" alt="" className='sm:w-8/12 md:w-11/12' />
+            <div className='flex items-center justify-center md:order-2 sm:w-8/12 sm:self-center md:w-6/12'>
+
+                <img src="./diploma.png" alt="" />
+
             </div>
 
-            <div className="flex flex-col justify-center gap-12 md:gap-6 md:row-span-1">
+            <div className='flex flex-col justify-center gap-12 md:order-3 md:w-6/12'>
 
-                <h2 className='text-white text-3xl font-semibold border-b-4 border-purple-500 pb-2 self-start'>Skills.</h2>
+                <h2 className='text-white text-2xl font-semibold border-b-4 border-purple-500 pb-2 self-start'>Skills.</h2>
 
-                <div className='grid grid-cols-4 items-center justify-items-center gap-8 md:grid-cols-6'>
+                <div className="grid grid-cols-4 items-center justify-items-center gap-8">
                     <i className='text-white text-4xl'><SiHtml5></SiHtml5></i>
                     <i className='text-white text-4xl'><SiCss3></SiCss3></i>
                     <i className='text-white text-4xl'><SiJavascript></SiJavascript></i>
@@ -37,6 +38,7 @@ const About = () => {
                     <i className='text-white text-4xl'><SiTailwindcss></SiTailwindcss></i>
                     <i className='text-white text-4xl'><SiFirebase></SiFirebase></i>
                 </div>
+
             </div>
 
         </div>
